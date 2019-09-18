11b69dfa07b8297a6a9644d382d5a125b79fbf62
Abel Rose <49218508+AbelRose@users.noreply.github.com>
2019-09-18 22:53:56 +0800

    Update Practice_9.java

--- Math/Practice_9.java
+++ Math/Practice_9.java
@@ -25,13 +25,15 @@ public class Practice_9 {
 
     //static 关键字的用途 方便在没有创建对象的时候调用 方法/变量
     //写在 main()方法外面
-    static ArrayList<User> users = new ArrayList<>();
+
+    static ArrayList<User> users = new ArrayList<>();//注意这个ArrayList 里面传的是User 返回的是一个User类的集合
     static {
         users.add(new User("张三","123"));
         users.add(new User("李四","456"));
         for (int i = 0; i < users.size(); i++) {
             users.get(i).show();
         }
+        System.out.println("--------");
     }
 
     public static void main(String[] args) {
@@ -41,8 +43,8 @@ public class Practice_9 {
         String name = sc.nextLine();
         System.out.println("请输入密码:");
         String pw = sc.nextLine();
-        User u = new User(name,pw);
-        String login = login(u);
+        User u = new User(name,pw);//传入一个有参的对象
+        String login = login(u);//创建一个方法 返回登陆是否成功 返回一个字符串
         System.out.println("登陆结果: "+login);
 
 
@@ -63,9 +65,29 @@ public class Practice_9 {
 
     private static String login(User u) {
         String msg = "";
-        String n = u.getName();
-        String p = u.getPassword();
+        String n = u.getName();//从static里面得来的用户名字
+        String p = u.getPassword();//从static里面得来的密码
 
-        
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);//定义一个用户并且取出名字和密码用来检验判断 传进来的User u 是否满足条件
+            String name = user.getName();
+            String password = user.getPassword();
+
+            boolean flag = true;
+                if (name.equals(n)) {//首先判断名字是否相同
+                    if (p.equals(password)) {//再判断密码是否相同
+                        return "登陆成功";
+                    } else {
+                        return "密码错误";
+                    }
+                } else {
+                    msg = "用户名不存在";
+//                continue;
+                }
+            }
+
+        return msg;
+
     }
 }
+
